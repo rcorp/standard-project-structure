e5a608152a9604b2ca7d1885b0e054920107ce59
neerajsachan <neerajsachan.biet@gmail.com>
2016-10-12 13:17:37 +0530

    created the Gruntfile.js, aliases.js for the grunt task and the mochaTest.js for the configuration file of the mochaTest cases in the generators/grunt folder

---
+++ Gruntfile.js
@@ -0,0 +1,5 @@
+const loadgruntFunction = require('load-grunt-config');
+
+module.exports = function loadgruntTask(grunt) {
+  loadgruntFunction(grunt);
+};
---
+++ generators/grunt/aliases.js
@@ -0,0 +1,6 @@
+module.exports = {
+  default: [],
+  build: [
+    'mochaTest',
+  ],
+};
