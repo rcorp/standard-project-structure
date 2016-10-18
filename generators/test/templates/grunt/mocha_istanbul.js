05ef3229c264fd248b0ff68b7f65bc649f7aaddb
neerajsachan <neerajsachan.biet@gmail.com>
2016-10-18 13:13:43 +0530

    add mocha_istanbul.js file in grunt folder to codecoverage

---
+++ generators/test/templates/grunt/mocha_istanbul.js
@@ -0,0 +1,7 @@
+module.exports = {
+  src: ['test/spec/unit/*.js'], // multiple folders also works
+  options: {
+    coverage: true, // this will make the grunt.event.on('coverage')
+    reportFormats: ['lcov'],
+  },
+};
