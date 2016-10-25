856b4fb0587795dcd033a0385f0da9763a8104d0
neerajsachan <neerajsachan.biet@gmail.com>
2016-10-25 12:41:57 +0530

    delete mocha_istanbul.js file

--- generators/test/templates/grunt/mocha_istanbul.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  src: ['test/spec/unit/*.js'], // multiple folders also works
-  options: {
-    coverage: true, // this will make the grunt.event.on('coverage')
-    reportFormats: ['lcov'],
-  },
-};
